f9d56603e6515be7c29065c9c89a7bbd459bbab8
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-01-11 00:30:11 +0100

    added reverse engineering notes and testing project

---
+++ re-testing/Testing/Testing/Hash.cs
@@ -0,0 +1,168 @@
+﻿namespace CodecTest;
+
+public static class Hash
+{
+    private static readonly byte[] Salt =
+    {
+        0xE7, 0xB2, 0x87, 0x76, 0xD1, 0xEF, 0x3F, 0xE0,
+        0xE0, 0x68, 0xE4, 0x1D, 0xD5, 0xC9, 0x57, 0xFF
+    };
+    
+    public static void GenerateHash(Span<byte> encryptedData, Span<byte> destination)
+    {
+        if (destination.Length != 16)
+            throw new ArgumentException("Destination needs to be 16 bytes long");
+
+        // in the game a static array is hashed before the encrypted data, but it's always 0 length so
+        // no point implementing it at this point
+        CalculateHash(encryptedData, destination);
+
+        Span<byte> hash2 = stackalloc byte[16];
+        
+        // 0-3 are always 0
+        hash2[..4].Fill(0);
+        
+        // 4-7 is where we'd put static hash len, but the game always sets it to 0 so we won't bother
+        // it uses the same shifts as the code for encrypted data length below
+        hash2[4..8].Fill(0);
+        
+        // 8-11 are always 0
+        hash2[8..12].Fill(0);
+
+        unchecked
+        {
+            hash2[12] = (byte)((uint)(8 * encryptedData.Length) >> 24);
+            hash2[13] = (byte)((uint)encryptedData.Length >> 13);
+            hash2[14] = (byte)((uint)encryptedData.Length >> 5);
+            hash2[15] = (byte)(8 * (uint)encryptedData.Length);
+        }
+
+        CalculateHash(hash2, destination);
+    }
+
+    // 0x7FF609E59D90
+    private static void CalculateHash(Span<byte> bytes, Span<byte> destination)
+    {
+        int srcPos = 0;
+        Span<byte> saltedBytes = stackalloc byte[16];
+        
+        if (bytes.Length >= 16)
+        {
+            for (int i = bytes.Length >> 4; i > 0; --i)
+            {
+                for (int j = 0; j < 16; ++j)
+                {
+                    destination[j] ^= bytes[srcPos + j];
+                }
+                
+                srcPos += 16;
+
+                SaltBytes(destination, saltedBytes);
+                saltedBytes.CopyTo(destination);
+            }
+        }
+
+        if (srcPos < bytes.Length)
+        {
+            int remaining = 16 - (bytes.Length - srcPos);
+
+            bytes[srcPos..bytes.Length].CopyTo(saltedBytes);
+            srcPos += bytes.Length - srcPos;
+            saltedBytes[(16 - remaining)..].Fill(0);
+
+            for (int i = 0; i < 16; ++i)
+            {
+                destination[i] ^= saltedBytes[i];
+            }
+
+            SaltBytes(destination, saltedBytes);
+            saltedBytes.CopyTo(destination);
+        }
+    }
+
+    // 0x7FF672EF9C20
+    private static void SaltBytes(Span<byte> bytes, Span<byte> destination)
+    {
+        Span<byte> saltCopy = stackalloc byte[16];
+    
+        destination.Fill(0); // fill destination with zeroes as it might be reused
+        Salt.CopyTo(saltCopy);
+
+        for (int i = 0; i < 16; ++i)
+        {
+            for (int j = 0; j < 8; ++j)
+            {
+                int v8 = (7 - j) switch
+                {
+                    0 => 1,
+                    1 => 2,
+                    2 => 4,
+                    3 => 8,
+                    4 => 16,
+                    5 => 32,
+                    6 => 64,
+                    7 => 128,
+                    _ => throw new InvalidOperationException("unreachable")
+                };
+
+                if ((v8 & bytes[i]) != 0)
+                {
+                    for (int k = 0; k < 16; ++k)
+                    {
+                        destination[k] ^= saltCopy[k];
+                    }
+                }
+
+                var lastSaltByte = saltCopy[15];
+                ShiftSaltBytes(saltCopy);
+
+                if ((lastSaltByte & 1) != 0)
+                {
+                    saltCopy[0] ^= 0xE1;
+                }
+            }
+        }
+    }
+
+    // 0x7FF609E597C0
+    private static void ShiftSaltBytes(Span<byte> bytes)
+    {
+        byte v2 = bytes[11];
+        uint v3 = (uint)(bytes[15] | ((bytes[14] | ((bytes[13] | (bytes[12] << 8)) << 8)) << 8)) >> 1;
+        uint v4 = v3 | 0x80000000;
+        
+        if ((v2 & 1) == 0)
+            v4 = v3;
+        
+        bytes[12] = (byte)((v4 >> 24) & 0xFF);
+        bytes[13] = (byte)((v4 >> 16) & 0xFF);
+        bytes[14] = (byte)((v4 >> 8) & 0xFF);
+        bytes[15] = (byte)(v4 & 0xFF);
+
+        uint v5 = ((uint)(bytes[11] | ((bytes[10] | ((bytes[9] | (bytes[8] << 8)) << 8)) << 8)) >> 1) | 0x80000000;
+        
+        if ((bytes[7] & 1) == 0)
+            v5 = (uint)(bytes[11] | ((bytes[10] | ((bytes[9] | (bytes[8] << 8)) << 8)) << 8)) >> 1;
+        
+        bytes[8] = (byte)(v5 >> 24);
+        bytes[9] = (byte)(v5 >> 16);
+        bytes[10] = (byte)(v5 >> 8);
+        bytes[11] = (byte)v5;
+
+        uint v6 = ((uint)(bytes[7] | ((bytes[6] | ((bytes[5] | (bytes[4] << 8)) << 8)) << 8)) >> 1) | 0x80000000;
+        
+        if ((bytes[3] & 1) == 0)
+            v6 = (uint)(bytes[7] | ((bytes[6] | ((bytes[5] | (bytes[4] << 8)) << 8)) << 8)) >> 1;
+        
+        bytes[4] = (byte)(v6 >> 24);
+        bytes[5] = (byte)(v6 >> 16);
+        bytes[6] = (byte)(v6 >> 8);
+        bytes[7] = (byte)v6;
+
+        uint v7 = (uint)(bytes[3] | ((bytes[2] | ((bytes[1] | (bytes[0] << 8)) << 8)) << 8)) >> 1;
+        bytes[0] = (byte)(v7 >> 24);
+        bytes[1] = (byte)(v7 >> 16);
+        bytes[2] = (byte)(v7 >> 8);
+        bytes[3] = (byte)v7;
+    }
+}
---
+++ re-testing/Testing/Testing/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics;
+using System.Text;
+using CodecTest;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+internal class Program
+{
+    // Test message
+    private const string B64Message = "pXCLBQnhkvI9/EUOsL6neoyaAocGq8q/zWViLKd+eO7v0azvPkUhAHGk3oC9cTnb1siObGwH73IWLdNPxck3ratY4DeIH1/TT+bT0Js3+KqyiCIboJKHIhP8UHkhnykeybdhGRTLyM2vTDRZ9Wd31zn5XkDBopdRYEIZ4C4XF6t/ux5cVeZFBbW5aWn9auSzF70jsm2Ffbuy2pU2WYzaZG5yIycMal8XzOJYEtjWBwaft2BCp4Lz/gHmqTwNoX+Fuv/qWQDzy2LDkY3NjfR2f7WjJHiV0m1ngdYealY4KvbLbc6qn2L2Qo2t/1t0+Ygz8z0QwO1ySLL81VGu0VWD+H9TO1J1TnyJ9w==";
+
+    private static readonly byte[] AesSecret =
+    {
+        0x4C, 0x48, 0x77, 0x55, 0x47, 0x6E, 0x6B, 0x74,
+        0x43, 0x6C, 0x4E, 0x76, 0x39, 0x55, 0x6F, 0x63,
+        0x31, 0x47, 0x71, 0x7A, 0x63, 0x63, 0x62, 0x68,
+        0x72, 0x64, 0x61, 0x4A, 0x33, 0x41, 0x06, 0x0A,
+    };
+
+    public static void Main(string[] args)
+    {
+        var messageBytes = Convert.FromBase64String(B64Message).AsMemory();
+        Span<byte> ivKey = stackalloc byte[16];
+        messageBytes.Span[..12].CopyTo(ivKey);
+        var encryptedData = messageBytes.Span[12..^16];
+        var encryptedHash = messageBytes.Span[^16..];
+
+        ivKey[15] = 2; // final iv key byte is 2 when encrypting/decrypting data
+        string decryptedData = EncryptDecryptString(encryptedData, AesSecret, ivKey);
+
+        var jObject = JsonConvert.DeserializeObject<JObject>(decryptedData);
+        decryptedData = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+
+        var hashSw = Stopwatch.StartNew();
+        Span<byte> hash = stackalloc byte[16];
+        Hash.GenerateHash(encryptedData, hash);
+        hashSw.Stop();
+
+        ivKey[15] = 1; // final iv key byte is 1 when encrypting/decrypting hash
+        var compareEncryptedHash = EncryptDecryptBytes(hash, AesSecret, ivKey);
+
+        Console.WriteLine(decryptedData);
+
+        if (CompareSpans(encryptedHash, compareEncryptedHash))
+        {
+            Console.WriteLine("The data is valid, hashes match");
+        }
+        else
+        {
+            Console.WriteLine($"The data is NOT valid, hashes do not match");
+        }
+    }
+
+    private static bool CompareSpans(Span<byte> a, Span<byte> b)
+    {
+        if (a.Length != b.Length)
+            return false;
+
+        for (int i = 0; i < a.Length; ++i)
+        {
+            if (a[i] != b[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    private static string EncryptDecryptString(Span<byte> data, Span<byte> secret, Span<byte> iv)
+    {
+        return Encoding.UTF8.GetString(EncryptDecryptBytes(data, secret, iv));
+    }
+
+    private static byte[] EncryptDecryptBytes(Span<byte> data, Span<byte> secret, Span<byte> iv)
+    {
+        using var aes = new AesCtr();
+        aes.Key = secret.ToArray();
+        aes.IV = iv.ToArray();
+
+        var decryptor = aes.CreateEncryptor(); // CTR uses same method to encrypt and decrypt
+        byte[] outputBuffer = new byte[data.Length];
+
+        decryptor.TransformBlock(data.ToArray(), 0, data.Length, outputBuffer, 0);
+
+        return outputBuffer;
+    }
+    
+    internal static string ByteArrayToString(Span<byte> bytes)
+    {
+        StringBuilder hex = new StringBuilder(bytes.Length * 3);
+        foreach (byte b in bytes)
+            hex.AppendFormat("{0:X2} ", b);
+        return hex.ToString().TrimEnd();
+    }
+}
