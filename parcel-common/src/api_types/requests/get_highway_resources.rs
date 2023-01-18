94a9919fb22feca9318808555bf0a3994245f1c2
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-01-18 19:42:36 +0100

    Added getHighwayResources request/response structs

---
+++ parcel-common/src/api_types/requests/get_highway_resources.rs
@@ -0,0 +1,57 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct GetHighwayResourcesRequest {
+    #[serde(rename = "cds")]
+    pub constructions: Vec<Construction>,
+    #[serde(rename = "rids")]
+    pub resource_ids: Vec<i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ConstructionRequest {
+    #[serde(rename = "cid")]
+    pub construction_id: i32,
+    /// This date is expressed as microseconds since 0001-01-01 00:00:00
+    #[serde(rename = "lgd")]
+    pub last_login_date: u64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct GetHighwayResourcesResponse {
+    #[serde(rename = "cs")]
+    pub construction_contributors: Vec<ConstructionContributors>,
+    #[serde(rename = "pr")]
+    pub put_resources: Vec<PutResource>,
+    #[serde(rename = "ul")]
+    pub users_like: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ConstructionContributors {
+    #[serde(rename = "cid")]
+    pub construction_id: i32,
+    #[serde(rename = "cs")]
+    pub contributors: Vec<Contributor>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Contributor {
+    #[serde(rename = "l")]
+    pub likes: i32,
+    #[serde(rename = "u")]
+    pub account_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct PutResource {
+    #[serde(rename = "cid")]
+    pub construction_id: i32,
+    #[serde(rename = "n")]
+    pub put_num: i64,
+    #[serde(rename = "rid")]
+    pub resource_id: i32,
+    /// Seems to always be 0
+    #[serde(rename = "un")]
+    pub users_put_num: i64,
+}
--- parcel-common/src/api_types/requests/mod.rs
+++ parcel-common/src/api_types/requests/mod.rs
@@ -1,4 +1,6 @@
+pub mod get_highway_resources;
 pub mod get_player_profile;
+pub mod get_ranking_schedules;
 pub mod get_version;
 pub mod lookup;
 pub mod reverse_lookup;
