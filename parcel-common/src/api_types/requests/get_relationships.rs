e085f80118fd0ef3ee0e6acf4aff8ea2c4fc0114
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-01-20 14:42:42 +0100

    Added getRelationships request/response structs

---
+++ parcel-common/src/api_types/requests/get_relationships.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct GetRelationshipsRequest;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct GetRelationshipResponse {
+    /// Last x people the player interacted with.
+    ///
+    /// The official server returns 10 players.
+    #[serde(rename = "history")]
+    history: Vec<RelationshipHistory>,
+    /// Players that we have a strand contract with.
+    #[serde(rename = "strand")]
+    strand_contracts: Vec<StrandContract>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct RelationshipHistory {
+    /// The time when this player was last interacted with
+    /// expressed as epoch (milliseconds)
+    #[serde(rename = "t")]
+    pub last_interaction_time: i64,
+    #[serde(rename = "uid")]
+    pub account_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct StrandContract {
+    /// The time when this strand contract was made
+    /// expressed as epoch (milliseconds)
+    #[serde(rename = "t")]
+    pub added_time: i64,
+    /// The account id of the player that we have a strand contract with
+    #[serde(rename = "uid")]
+    pub account_id: String,
+}
--- parcel-common/src/api_types/requests/mod.rs
+++ parcel-common/src/api_types/requests/mod.rs
@@ -3,6 +3,7 @@ pub mod get_highway_resources;
 pub mod get_like_history;
 pub mod get_player_profile;
 pub mod get_ranking_schedules;
+pub mod get_relationships;
 pub mod get_version;
 pub mod lookup;
 pub mod reverse_lookup;
