c076875d62b10a2831f5dc4f66f2eafe034d46dc
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-01-26 00:21:55 +0100

    Added findQpidObjects request/response structs

---
+++ parcel-common/src/api_types/requests/find_qpid_objects.rs
@@ -0,0 +1,58 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+use crate::api_types::{area::AreaHash, object::QpidObjectsResponse};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct FindQpidObjectsRequest {
+    #[serde(rename = "aid")]
+    pub area_hash: AreaHash,
+    #[serde(rename = "q")]
+    pub qpid_id: i32,
+    /// Account ids of people we want to prioritize finding objects from
+    #[serde(rename = "u")]
+    pub account_ids: Vec<String>,
+    #[serde(rename = "o")]
+    pub object: Option<ObjectRequest>,
+    #[serde(rename = "ro")]
+    pub road: Option<RoadRequest>,
+    #[serde(rename = "m")]
+    pub mission: Option<MissionRequest>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ObjectRequest {
+    #[serde(rename = "ct")]
+    pub counts: HashMap<String, i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct RoadRequest {
+    #[serde(rename = "c")]
+    pub count: i32,
+    #[serde(rename = "plid")]
+    pub prioritized_location_id: i32,
+    #[serde(rename = "qe")]
+    pub end_qpids: Vec<i32>,
+    #[serde(rename = "rlid")]
+    pub required_location_id: i32,
+    #[serde(rename = "ver")]
+    pub data_version: i32,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct MissionRequest {
+    #[serde(rename = "areaId")]
+    pub area_hash: AreaHash,
+    #[serde(rename = "limit")]
+    pub limit: u32,
+    #[serde(rename = "qids")]
+    pub qpid_ids: Vec<i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct FindQpidObjectsResponse {
+    #[serde(rename = "n")]
+    pub normal: QpidObjectsResponse,
+}
--- parcel-common/src/api_types/requests/mod.rs
+++ parcel-common/src/api_types/requests/mod.rs
@@ -5,6 +5,7 @@ pub mod delete_missions;
 pub mod delete_object;
 pub mod devote_highway_resources;
 pub mod find_missions;
+pub mod find_qpid_objects;
 pub mod get_highway_resources;
 pub mod get_like_history;
 pub mod get_ordered_missions;
