ac486a26940c20802dcc168d489d39111de0adca
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-03 16:15:02 +0100

    Return version info from getVersion endpoint

--- parcel-server/src/endpoints/get_version.rs
+++ parcel-server/src/endpoints/get_version.rs
@@ -1,6 +1,7 @@
 use actix_web::{post, HttpResponse, Responder};
+use parcel_common::api_types::requests::get_version::GetVersionResponse;
 
 #[post("/getVersion")]
 pub async fn get_version() -> impl Responder {
-    HttpResponse::InternalServerError().body("not implemented")
+    HttpResponse::Ok().json(GetVersionResponse::current_version())
 }
