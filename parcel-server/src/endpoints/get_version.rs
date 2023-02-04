2fe55df46fae31caa15b3ab171adb5e8e53b72f4
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-04 16:15:11 +0100

    Changed getVersion endpoint from POST to GET

--- parcel-server/src/endpoints/get_version.rs
+++ parcel-server/src/endpoints/get_version.rs
@@ -1,7 +1,7 @@
-use actix_web::{post, HttpResponse, Responder};
+use actix_web::{get, HttpResponse, Responder};
 use parcel_common::api_types::requests::get_version::GetVersionResponse;
 
-#[post("/getVersion")]
+#[get("/getVersion")]
 pub async fn get_version() -> impl Responder {
     HttpResponse::Ok().json(GetVersionResponse::current_version())
 }
