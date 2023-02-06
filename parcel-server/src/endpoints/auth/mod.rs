22d99af8e95ee776788f0d075b572746f8a2cc00
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-06 01:32:14 +0100

    Changed /ds/auth method to GET and added steam ticket verification

--- parcel-server/src/endpoints/auth/mod.rs
+++ parcel-server/src/endpoints/auth/mod.rs
@@ -1,46 +1,71 @@
 use std::fmt::Display;
 
+use actix_http::StatusCode;
 use actix_web::{
-    post,
+    get,
     web::{Data, Query},
     HttpResponse, Responder, ResponseError,
 };
 use parcel_common::api_types::auth::Provider;
 use serde::Deserialize;
 
-use crate::data::steam::Steam;
+use crate::data::steam::{Steam, VerifyUserAuthTicketError};
 
 #[derive(Debug, Deserialize)]
 pub struct AuthQuery {
     provider: Provider,
-    display_name: String,
+    #[serde(rename = "display_name")]
+    _display_name: String,
     code: String,
 }
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     UnsupportedPlatform,
+    ApiResponseError(anyhow::Error),
+    InvalidCode,
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::UnsupportedPlatform => write!(f, "The provided platform is not supported"),
+            Error::ApiResponseError(err) => {
+                write!(
+                    f,
+                    "An error occured while sending a request to an external api: {:?}",
+                    err
+                )
+            }
+            Error::InvalidCode => {
+                write!(f, "Could not authenticate user from code")
+            }
         }
     }
 }
 
 impl ResponseError for Error {
-    fn status_code(&self) -> actix_http::StatusCode {
-        actix_http::StatusCode::INTERNAL_SERVER_ERROR
+    fn status_code(&self) -> StatusCode {
+        match self {
+            Error::UnsupportedPlatform => StatusCode::BAD_REQUEST,
+            Error::ApiResponseError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            Error::InvalidCode => StatusCode::UNAUTHORIZED,
+        }
     }
 }
 
-#[post("auth/ds")]
+#[get("auth/ds")]
 pub async fn auth(request: Query<AuthQuery>, steam: Data<Steam>) -> Result<impl Responder, Error> {
     match &request.provider {
         Provider::Steam => {
-            // todo
+            let user_info = steam
+                .verify_user_auth_ticket(&request.code)
+                .await
+                .map_err(|err| match err {
+                    VerifyUserAuthTicketError::InvalidTicket => Error::InvalidCode,
+                    other => Error::ApiResponseError(other.into()),
+                })?;
+
             Ok(HttpResponse::InternalServerError().body("not implemented"))
         }
         _ => Err(Error::UnsupportedPlatform),
