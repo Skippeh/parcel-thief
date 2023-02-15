370c716e3132bedf2157e7fe2341cb216218d4d6
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-15 01:49:41 +0100

    Changed workflow of thief-injector: Wait for process again if it dies, and inject again. Pressing enter still ejects and exits the process.

--- parcel-thief/thief-injector/src/main.rs
+++ parcel-thief/thief-injector/src/main.rs
@@ -1,47 +1,102 @@
-use std::io::BufRead;
+use std::{path::PathBuf, time::Duration};
 
-use dll_syringe::{process::OwnedProcess, Syringe};
+use clap::Parser;
+use crossterm::event::{Event, EventStream, KeyEventKind};
+use dll_syringe::{
+    process::{BorrowedProcess, OwnedProcess, Process},
+    Syringe,
+};
+use futures::StreamExt;
+use tokio::{select, time::sleep};
 
-fn main() {
-    static PROCESS_NAME: &str = "ds";
-    static DLL_PATH: &str = "../../target/x86_64-pc-windows-msvc/release/parcel_thief.dll";
+#[derive(Parser)]
+struct Options {
+    #[clap(default_value = "parcel-thief.dll")]
+    dll_path: PathBuf,
+}
 
-    println!("waiting for {PROCESS_NAME}");
+#[tokio::main]
+async fn main() {
+    let args = Options::parse();
+
+    if !args.dll_path.exists() {
+        println!("Dll file does not exist: {}", args.dll_path.display());
+        return;
+    }
+
+    if !args.dll_path.is_file() {
+        println!("Dll path is not a file: {}", args.dll_path.display());
+        return;
+    }
+
+    println!("Press any key to eject and exit at anytime.");
 
     loop {
-        let target_process = match OwnedProcess::find_first_by_name(PROCESS_NAME) {
-            Some(target) => target,
-            None => {
-                std::thread::sleep(std::time::Duration::from_millis(100));
-                continue;
+        let target_process = OwnedProcess::find_first_by_name("ds");
+
+        if let Some(target_process) = target_process {
+            println!("Found process, injecting...");
+
+            let syringe = Syringe::for_process(target_process);
+
+            let injected_payload = match syringe.inject(&args.dll_path) {
+                Ok(payload) => payload,
+                Err(err) => {
+                    println!("Failed to inject: {err:?}");
+                    break;
+                }
+            };
+
+            println!("Successfully injected");
+
+            select! {
+                _ = wait_for_next_key() => {}
+                _ = poll_process_exit(syringe.process()) => {
+                    println!("Process died, waiting again for ds...");
+                    sleep(Duration::from_secs(1)).await; // wait a bit for process to exit completely
+                    continue;
+                }
+            };
+
+            println!("Ejecting...");
+
+            match syringe.eject(injected_payload) {
+                Ok(_) => println!("Ejected successfully"),
+                Err(err) => {
+                    println!("Failed to eject: {err:?}");
+                }
+            };
+
+            break;
+        } else {
+            select! {
+                _ = wait_for_next_key() => { break; }
+                _ = sleep(Duration::from_millis(100)) => {}
             }
-        };
+        }
+    }
+}
 
-        println!("found process, injecting");
+async fn poll_process_exit(process: BorrowedProcess<'_>) {
+    loop {
+        if process.is_alive() {
+            sleep(Duration::from_millis(100)).await;
+        } else {
+            break;
+        }
+    }
+}
 
-        let syringe = Syringe::for_process(target_process);
+async fn wait_for_next_key() {
+    let mut reader = EventStream::new();
 
-        let injected_payload = match syringe.inject(DLL_PATH) {
-            Ok(payload) => payload,
-            Err(err) => {
-                println!("failed to inject {err:?}");
-                continue;
+    loop {
+        let event = reader.next().await;
+
+        if let Some(Ok(Event::Key(key))) = event {
+            if key.kind == KeyEventKind::Press {
+                break;
             }
-        };
-
-        println!("injected! press enter to eject");
-
-        let stdin = std::io::stdin();
-        let mut lines_iter = stdin.lock().lines();
-        let _ = lines_iter.next();
-
-        println!("ejecting");
-
-        match syringe.eject(injected_payload) {
-            Ok(_) => println!("ejected successfully"),
-            Err(err) => println!("failed to eject {err:?}"),
-        };
-
-        break;
+        }
     }
 }
