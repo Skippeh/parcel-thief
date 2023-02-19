294ca368da4bf7245813c3e70c8fdf0f92663685
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-19 15:33:30 +0100

    Added authentication requirement to /ds/e/getVersion

--- parcel-server/src/endpoints/get_version.rs
+++ parcel-server/src/endpoints/get_version.rs
@@ -1,7 +1,9 @@
 use actix_web::{get, HttpResponse, Responder};
 use parcel_common::api_types::requests::get_version::GetVersionResponse;
 
+use crate::session::Session;
+
 #[get("/getVersion")]
-pub async fn get_version() -> impl Responder {
+pub async fn get_version(_session: Session) -> impl Responder {
     HttpResponse::Ok().json(GetVersionResponse::current_version())
 }
