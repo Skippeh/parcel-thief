9541c0211184f3a1c2b90e5d103547b4f5975678
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-20 19:48:11 +0100

    Made getOrderedMissions request object optional

--- parcel-server/src/endpoints/get_ordered_missions.rs
+++ parcel-server/src/endpoints/get_ordered_missions.rs
@@ -2,6 +2,8 @@ use actix_web::{post, web::Json, HttpResponse, Responder};
 use parcel_common::api_types::requests::get_ordered_missions::GetOrderedMissionsRequest;
 
 #[post("getOrderedMissions")]
-pub async fn get_ordered_missions(request: Json<GetOrderedMissionsRequest>) -> impl Responder {
+pub async fn get_ordered_missions(
+    request: Option<Json<GetOrderedMissionsRequest>>,
+) -> impl Responder {
     HttpResponse::InternalServerError().body("not implemented")
 }
