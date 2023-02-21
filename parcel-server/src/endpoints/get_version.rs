ba55d4a98b38441f01969ab5cd1d4e7122948667
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-21 01:12:28 +0100

    Constricted getVersion endpoint return type

--- parcel-server/src/endpoints/get_version.rs
+++ parcel-server/src/endpoints/get_version.rs
@@ -1,9 +1,9 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, web::Json};
 use parcel_common::api_types::requests::get_version::GetVersionResponse;
 
 use crate::session::Session;
 
 #[get("/getVersion")]
-pub async fn get_version(_session: Session) -> impl Responder {
-    HttpResponse::Ok().json(GetVersionResponse::current_version())
+pub async fn get_version(_session: Session) -> Json<GetVersionResponse> {
+    Json(GetVersionResponse::current_version())
 }
