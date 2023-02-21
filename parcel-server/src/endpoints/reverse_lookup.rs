b405d4341ca41a865a66509a1f56be38dbce4bd7
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-21 01:16:06 +0100

    Implemented reverseLookup endpoint

--- parcel-server/src/endpoints/reverse_lookup.rs
+++ parcel-server/src/endpoints/reverse_lookup.rs
@@ -1,16 +1,32 @@
-use actix_web::{post, web::Json, Responder};
+use actix_web::{
+    post,
+    web::{Data, Json},
+};
+
 use parcel_common::api_types::requests::reverse_lookup::{
     ReverseLookupRequest, ReverseLookupResponse,
 };
 
-use crate::session::Session;
+use crate::{data::database::Database, endpoints::InternalError, session::Session};
 
+/// This request takes a list of provider account ids and returns the account id.
+///
+/// If an account id does not have an account it's not added to the response.
 #[post("reverseLookup")]
 pub async fn reverse_lookup(
     request: Json<ReverseLookupRequest>,
     _session: Session,
-) -> impl Responder {
-    Json(ReverseLookupResponse {
-        account_ids: Vec::new(),
-    })
+    database: Data<Database>,
+) -> Result<Json<ReverseLookupResponse>, InternalError> {
+    let conn = database.connect()?;
+    let accounts = conn.accounts();
+
+    let account_ids = accounts
+        .get_by_provider_ids(request.provider, &request.provider_account_ids)
+        .await?
+        .into_iter()
+        .map(|acc| acc.id)
+        .collect();
+
+    Ok(Json(ReverseLookupResponse { account_ids }))
 }
