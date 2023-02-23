27824a3b780fe0aed1c7192e0a68b288e8481d68
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-23 16:35:42 +0100

    Implemented getPlayerProfile and setPlayerProfile endpoints

--- parcel-server/src/endpoints/get_player_profile.rs
+++ parcel-server/src/endpoints/get_player_profile.rs
@@ -1,7 +1,110 @@
-use actix_web::{post, web::Json, HttpResponse, Responder};
-use parcel_common::api_types::requests::get_player_profile::GetPlayerProfileRequest;
+use std::collections::HashMap;
+
+use actix_web::{
+    post,
+    web::{Data, Json},
+};
+use parcel_common::api_types::{
+    auth::Provider,
+    player_profile::BasicPlayerProfile,
+    requests::get_player_profile::{
+        GetPlayerProfileRequest, GetPlayerProfileResponse, ProfileResponse,
+    },
+};
+
+use crate::{data::database::Database, endpoints::InternalError, session::Session};
 
 #[post("getPlayerProfile")]
-pub async fn get_player_profile(request: Json<GetPlayerProfileRequest>) -> impl Responder {
-    HttpResponse::InternalServerError().body("not implemented")
+pub async fn get_player_profile(
+    request: Json<GetPlayerProfileRequest>,
+    database: Data<Database>,
+    _session: Session,
+) -> Result<Json<GetPlayerProfileResponse>, InternalError> {
+    if request.profiles.is_empty() {
+        return Ok(Json(GetPlayerProfileResponse { profiles: vec![] }));
+    }
+
+    let conn = database.connect()?;
+    let accounts = conn.accounts();
+    let profiles = conn.player_profiles();
+
+    // Make sure all flags equal 1 or return error
+    // (Not sure if flags are ever any other value)
+    for profile in &request.profiles {
+        if profile.flags != 1 {
+            return Err(anyhow::anyhow!("Unknown profile flags: {}", profile.flags).into());
+        }
+    }
+
+    // The owned string is the account id, and the borrowed string is the request id, which is returned in the response
+    let mut account_ids = HashMap::<String, &str>::new();
+
+    // non account ids to query, grouped by provider type and coupled with their original request id (used in response)
+    // the hashmap key and value is (provider_id, original_id)
+    let mut id_queries = HashMap::<Provider, HashMap<&str, &str>>::new();
+
+    for profile in &request.profiles {
+        if let Some((provider_str, id)) = profile.id.split_once('_') {
+            match provider_str {
+                "zygo" => {
+                    account_ids.insert(profile.id.clone(), &profile.id);
+                }
+                "steam" => {
+                    id_queries
+                        .entry(Provider::Steam)
+                        .or_insert(HashMap::new())
+                        .insert(id, &profile.id);
+                }
+                "epic" => {
+                    id_queries
+                        .entry(Provider::Epic)
+                        .or_insert(HashMap::new())
+                        .insert(id, &profile.id);
+                }
+                other => return Err(anyhow::anyhow!("Unexpected id type: {:?}", other).into()),
+            }
+        }
+    }
+
+    // Query for account ids
+    for (provider, provider_ids_with_original_id) in &id_queries {
+        let provider_ids: Vec<&str> = provider_ids_with_original_id
+            .iter()
+            .map(|(provider_id, _)| *provider_id)
+            .collect();
+
+        for account in accounts
+            .get_by_provider_ids(*provider, &provider_ids)
+            .await?
+        {
+            let original_id = *provider_ids_with_original_id
+                .get(account.provider_id.as_str())
+                .unwrap();
+            account_ids.insert(account.id, original_id);
+        }
+    }
+
+    // Query db and transform results into response types
+    let profiles: Vec<ProfileResponse> = profiles
+        .get_by_account_ids(
+            &account_ids
+                .keys()
+                .map(|id| id.as_str())
+                .collect::<Vec<&str>>(),
+        )
+        .await?
+        .into_iter()
+        .map(|profile| {
+            let original_id = account_ids
+                .get(&profile.account_id)
+                .map(|s| (*s).to_owned())
+                .unwrap();
+            ProfileResponse {
+                basic: BasicPlayerProfile::from(profile),
+                id: original_id,
+            }
+        })
+        .collect();
+
+    Ok(Json(GetPlayerProfileResponse { profiles }))
 }
--- parcel-server/src/endpoints/set_player_profile.rs
+++ parcel-server/src/endpoints/set_player_profile.rs
@@ -1,17 +1,93 @@
-use actix_web::{put, web::Json, Responder};
+use std::fmt::Display;
+
+use actix_web::{
+    put,
+    web::{Data, Json},
+};
+use diesel::ConnectionError;
 use parcel_common::api_types::requests::set_player_profile::{
     SetPlayerProfileRequest, SetPlayerProfileResponse,
 };
 
-use crate::session::Session;
+use crate::{
+    data::database::Database,
+    db::{models::player_profile::PlayerProfile, QueryError},
+    response_error::{impl_response_error, CommonResponseError},
+    session::Session,
+};
+
+use super::InternalError;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    InternalError(InternalError),
+    InvalidProfileValue(anyhow::Error),
+}
+
+impl From<QueryError> for Error {
+    fn from(value: QueryError) -> Self {
+        Self::InternalError(value.into())
+    }
+}
+
+impl From<ConnectionError> for Error {
+    fn from(value: ConnectionError) -> Self {
+        Self::InternalError(value.into())
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::InternalError(err) => err.fmt(f),
+            Error::InvalidProfileValue(err) => write!(f, "Invalid profile value: {}", err),
+        }
+    }
+}
+
+impl_response_error!(Error);
+impl CommonResponseError for Error {
+    fn get_status_code(&self) -> String {
+        match self {
+            Error::InternalError(err) => err.get_status_code(),
+            Error::InvalidProfileValue(_) => "SPP_IPV".into(),
+        }
+    }
+
+    fn get_http_status_code(&self) -> actix_http::StatusCode {
+        match self {
+            Error::InternalError(err) => err.get_http_status_code(),
+            Error::InvalidProfileValue(_) => actix_http::StatusCode::BAD_REQUEST,
+        }
+    }
+
+    fn get_message(&self) -> String {
+        match self {
+            Error::InternalError(err) => err.get_message(),
+            Error::InvalidProfileValue(_) => "invalid profile values".into(),
+        }
+    }
+}
 
 #[put("setPlayerProfile")]
 pub async fn set_player_profile(
     request: Json<SetPlayerProfileRequest>,
     session: Session,
-) -> impl Responder {
-    Json(SetPlayerProfileResponse {
-        account_id: session.account_id,
-        basic: request.0.basic,
-    })
+    database: Data<Database>,
+) -> Result<Json<SetPlayerProfileResponse>, Error> {
+    let account_id = session.account_id.clone();
+    let conn = database.connect()?;
+    let profiles = conn.player_profiles();
+
+    profiles
+        .add_or_update_profile(
+            &PlayerProfile::try_from((account_id.clone(), &request.basic))
+                .map_err(|err| Error::InvalidProfileValue(err.into()))?,
+        )
+        .await?;
+
+    Ok(Json(SetPlayerProfileResponse {
+        account_id,
+        basic: request.into_inner().basic,
+    }))
 }
