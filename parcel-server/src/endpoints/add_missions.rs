c4aea9143fb217293d0716c8d943f88a961aaa00
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-28 00:48:57 +0100

    Changed addMissions endpoint http method to PUT

--- parcel-server/src/endpoints/add_missions.rs
+++ parcel-server/src/endpoints/add_missions.rs
@@ -1,7 +1,7 @@
-use actix_web::{post, web::Json, HttpResponse, Responder};
+use actix_web::{put, web::Json, HttpResponse, Responder};
 use parcel_common::api_types::requests::add_missions::AddMissionsRequest;
 
-#[post("addMissions")]
+#[put("addMissions")]
 pub async fn add_missions(request: Json<AddMissionsRequest>) -> impl Responder {
     HttpResponse::InternalServerError().body("not implemented")
 }
