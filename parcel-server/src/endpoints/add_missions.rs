dcce880223d5a257eea891ef20d1ce68c56c5c8f
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-02-28 01:35:21 +0100

    Constrained return type of addMissions endpoint

--- parcel-server/src/endpoints/add_missions.rs
+++ parcel-server/src/endpoints/add_missions.rs
@@ -1,7 +1,11 @@
-use actix_web::{put, web::Json, HttpResponse, Responder};
-use parcel_common::api_types::requests::add_missions::AddMissionsRequest;
+use actix_web::{put, web::Json};
+use parcel_common::api_types::requests::add_missions::{AddMissionsRequest, AddMissionsResponse};
+
+use crate::endpoints::InternalError;
 
 #[put("addMissions")]
-pub async fn add_missions(request: Json<AddMissionsRequest>) -> impl Responder {
-    HttpResponse::InternalServerError().body("not implemented")
+pub async fn add_missions(
+    request: Json<AddMissionsRequest>,
+) -> Result<Json<AddMissionsResponse>, InternalError> {
+    Err(anyhow::anyhow!("Not implemented").into())
 }
