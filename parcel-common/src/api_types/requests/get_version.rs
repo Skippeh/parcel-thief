33f95cf573323f1c171ed7aaf15abe9d841f3315
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-04-28 01:58:29 +0200

    Moved current version info to the endpoint instead of next to the struct definition

--- parcel-common/src/api_types/requests/get_version.rs
+++ parcel-common/src/api_types/requests/get_version.rs
@@ -16,18 +16,5 @@ pub struct GetVersionResponse {
     pub version: String,
     /// Is normally "ds", should probably always be
     #[serde(rename = "zone")]
-    zone: String,
-}
-
-impl GetVersionResponse {
-    /// Gets the version that matches the real game server as of 2023-01-18
-    pub fn current_version() -> Self {
-        Self {
-            domain: "prod".into(),
-            major: 0,
-            minor: 12,
-            version: "0.0.1".into(),
-            zone: "ds".into(),
-        }
-    }
+    pub zone: String,
 }
--- parcel-server/src/endpoints/get_version.rs
+++ parcel-server/src/endpoints/get_version.rs
@@ -5,5 +5,11 @@ use crate::session::Session;
 
 #[get("/getVersion")]
 pub async fn get_version(_session: Session) -> Json<GetVersionResponse> {
-    Json(GetVersionResponse::current_version())
+    Json(GetVersionResponse {
+        domain: "prod".into(),
+        major: 0,
+        minor: 12,
+        version: "0.0.1".into(),
+        zone: "ds".into(),
+    })
 }
