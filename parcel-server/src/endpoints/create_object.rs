a1a5d835cd11cb657c4740bd5c05b26f89eebe3a
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-06-13 22:31:05 +0200

    Guard against invalid object type format when the type is unknown

--- parcel-server/src/endpoints/create_object.rs
+++ parcel-server/src/endpoints/create_object.rs
@@ -3,8 +3,9 @@ use actix_web::{
     web::{Data, Json},
 };
 
-use parcel_common::api_types::requests::create_object::{
-    CreateObjectRequest, CreateObjectResponse,
+use parcel_common::api_types::{
+    object::ObjectType,
+    requests::create_object::{CreateObjectRequest, CreateObjectResponse},
 };
 
 use crate::{data::database::Database, endpoints::InternalError, session::Session};
@@ -15,6 +16,14 @@ pub async fn create_object(
     database: Data<Database>,
     session: Session,
 ) -> Result<Json<CreateObjectResponse>, InternalError> {
+    // Make sure that if object_type is unknown, then it is exactly 1 character long
+    // todo: return bad request error instead of internal error
+    if let ObjectType::Unknown(val) = &request.object_type {
+        if val.len() != 1 {
+            return Err(anyhow::anyhow!("Invalid object type: {}", val).into());
+        }
+    }
+
     let db = database.connect()?;
     let qpid_objects = db.qpid_objects();
     let result = qpid_objects
