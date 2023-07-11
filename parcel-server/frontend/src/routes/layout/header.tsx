552bb717aadc37ba4d4aa88419aeee24c2915eea
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-07-11 03:48:56 +0200

    Removed sidebar and added nav menu to header

--- parcel-server/frontend/src/routes/layout/header.tsx
+++ parcel-server/frontend/src/routes/layout/header.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
 import styled from "styled-components";
 import useSession from "../../hooks/use_session";
+import { NavLink } from "react-router-dom";
 
 const Wrapper = styled.div`
   grid-area: 1 / 1 / 1 / 3;
 
   background: rgb(31, 37, 55);
   display: flex;
-  justify-content: right;
+  justify-content: space-between;
   align-items: center;
-  padding: 1rem;
 
   & .user {
+    padding: 1rem;
     display: flex;
     align-items: center;
     gap: 0.75rem;
@@ -32,6 +33,33 @@ const Wrapper = styled.div`
       }
     }
   }
+
+  & nav {
+    padding: 0 1rem;
+
+    & ul {
+      list-style: none;
+      padding: 0;
+
+      & li {
+        display: inline-block;
+
+        & a {
+          text-decoration: none;
+          color: #f5f5f5;
+          padding: 1rem;
+
+          &.active {
+            background: #257524;
+          }
+
+          &:not(.active):hover {
+            background: rgba(255, 255, 255, 0.1);
+          }
+        }
+      }
+    }
+  }
 `;
 
 const Header = () => {
@@ -42,6 +70,13 @@ const Header = () => {
 
   return (
     <Wrapper>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+        </ul>
+      </nav>
       <div className="user">
         <div className="name">{user.name}</div>
         <div className="avatar">
--- parcel-server/frontend/src/routes/layout/index.tsx
+++ parcel-server/frontend/src/routes/layout/index.tsx
@@ -6,28 +6,20 @@ import Header from "./header";
 const Wrapper = styled.div`
   height: 100%;
   display: grid;
-  grid-template-columns: 20rem 1fr;
+  grid-template-columns: 1fr;
   grid-template-rows: 0fr 1fr;
   grid-column-gap: 0;
   grid-row-gap: 0;
 `;
 
-const Sidebar = styled.div`
-  grid-area: 2 / 1 / 2 / 1;
-
-  background: rgb(27, 33, 49);
-  border-right: 1px solid rgb(41, 45, 57);
-`;
-
 const Content = styled.div`
-  grid-area: 2 / 2 / 2 / 2;
+  grid-area: 2 / 1 / 2 / 2;
 `;
 
 const Layout = () => {
   return (
     <Wrapper>
       <Header />
-      <Sidebar>Sidebar</Sidebar>
       <Content>
         <Outlet />
       </Content>
