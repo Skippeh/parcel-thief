9b0aaf86697c4cac5b4d0b7b178b5ea0df0bfc27
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-07-14 04:42:59 +0200

    Fixed parcel-common compiler error if ts feature isn't enabled

--- parcel-common/src/api_types/frontend/auth.rs
+++ parcel-common/src/api_types/frontend/auth.rs
@@ -1,21 +1,26 @@
 use flagset::flags;
+
+#[cfg(feature = "ts")]
 use typescript_type_def::TypeDef;
 
 use crate::api_types::auth::Provider;
 
-#[derive(Debug, serde::Deserialize, TypeDef)]
+#[derive(Debug, serde::Deserialize)]
+#[cfg_attr(feature = "ts", derive(TypeDef))]
 #[serde(rename_all = "camelCase")]
 pub struct AuthRequest {
     pub provider: Provider,
 }
 
-#[derive(Debug, serde::Serialize, TypeDef)]
+#[derive(Debug, serde::Serialize)]
+#[cfg_attr(feature = "ts", derive(TypeDef))]
 #[serde(rename_all = "camelCase")]
 pub struct InitAuthResponse {
     pub redirect_url: String,
 }
 
-#[derive(Debug, serde::Deserialize, TypeDef)]
+#[derive(Debug, serde::Deserialize)]
+#[cfg_attr(feature = "ts", derive(TypeDef))]
 #[serde(rename_all = "camelCase")]
 pub struct CheckAuthRequest {
     pub callback_token: String,
@@ -23,14 +28,16 @@ pub struct CheckAuthRequest {
 
 flags! {
     // note: at the moment the typescript generator doesn't support serde_repr/c style enums. So this is currently unusable in TS without a workaround
-    #[derive(PartialOrd, Ord, Hash, serde_repr::Serialize_repr, serde_repr::Deserialize_repr, TypeDef)]
+    #[derive(PartialOrd, Ord, Hash, serde_repr::Serialize_repr, serde_repr::Deserialize_repr)]
+    #[cfg_attr(feature = "ts", derive(TypeDef))]
     #[repr(u32)]
     pub enum JwtPermissions: u32 {
         None = 0,
     }
 }
 
-#[derive(Debug, serde::Serialize, TypeDef, Clone)]
+#[derive(Debug, Clone, serde::Serialize)]
+#[cfg_attr(feature = "ts", derive(TypeDef))]
 #[serde(rename_all = "camelCase", tag = "type")]
 pub enum CheckAuthResponse {
     #[serde(rename_all = "camelCase")]
@@ -45,7 +52,8 @@ pub enum CheckAuthResponse {
     },
 }
 
-#[derive(Debug, serde::Deserialize, serde::Serialize, TypeDef)]
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+#[cfg_attr(feature = "ts", derive(TypeDef))]
 #[serde(rename_all = "camelCase")]
 pub struct JwtPayload {
     pub expires_at: i64,
