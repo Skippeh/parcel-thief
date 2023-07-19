a9f1ed26d00ee2661a74211e44e8b3fcd0522336
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-07-19 04:23:19 +0200

    Added form for account creation

--- parcel-server/frontend/src/routes/pages/accounts/frontend/account/create_local_account_button.tsx
+++ parcel-server/frontend/src/routes/pages/accounts/frontend/account/create_local_account_button.tsx
@@ -1,9 +1,31 @@
 import * as React from "react";
 import InfoText from "../../../../../components/info_text";
 import * as Dialog from "../../../../../components/dialog";
+import * as Form from "../../../../../components/form";
+import { styled } from "styled-components";
+import PasswordInput from "../../../../../components/password_input";
+
+const FormSubmit = styled(Form.Submit)`
+  margin-top: 0.3rem;
+`;
+
+const FormRoot = styled(Form.Root)`
+  width: 300px;
+`;
 
 const CreateLocalAccountButton = () => {
   const [open, setOpen] = React.useState(false);
+
+  function onSubmit(ev: React.FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.currentTarget);
+    console.log(formData);
+  }
+
+  function checkPasswordConfirm(value: string, formData: FormData) {
+    return value !== formData.get("password");
+  }
 
   return (
     <>
@@ -13,11 +35,44 @@ const CreateLocalAccountButton = () => {
           <Dialog.Overlay />
           <Dialog.Content>
             <Dialog.Title>Create local account</Dialog.Title>
-
-            <Dialog.Buttons>
-              <button className="primary">Create</button>
-              <Dialog.Close className="secondary">Cancel</Dialog.Close>
-            </Dialog.Buttons>
+            <FormRoot onSubmit={onSubmit}>
+              <Form.Field name="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="username"
+                  autoComplete="off"
+                  required
+                />
+              </Form.Field>
+              <Form.Field name="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  autoComplete="new-password"
+                  required
+                  minLength={8}
+                />
+                <Form.Message match="tooShort">
+                  Password must be at least 8 characters long
+                </Form.Message>
+              </Form.Field>
+              <Form.Field name="passwordConfirm">
+                <Form.Label>Confirm password</Form.Label>
+                <Form.Control
+                  type="password"
+                  autoComplete="new-password"
+                  required
+                />
+                <Form.Message match={checkPasswordConfirm}>
+                  Passwords do not match
+                </Form.Message>
+              </Form.Field>
+              <Dialog.Buttons>
+                <FormSubmit>Create</FormSubmit>
+                <Dialog.Close className="secondary">Cancel</Dialog.Close>
+              </Dialog.Buttons>
+            </FormRoot>
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
