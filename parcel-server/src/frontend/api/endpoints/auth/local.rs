5b8504538eb98bad2259abaeb3555a531eeb612c
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-07-20 15:21:44 +0200

    Added name and avatar to auth response when logging in with credentials

--- parcel-server/src/frontend/api/endpoints/auth/local.rs
+++ parcel-server/src/frontend/api/endpoints/auth/local.rs
@@ -3,10 +3,15 @@ use actix_web::{
     web::{Data, Json},
 };
 use flagset::FlagSet;
-use parcel_common::api_types::frontend::auth::{AuthAccountInfo, LocalAuthRequest};
+use parcel_common::api_types::{
+    auth::Provider,
+    frontend::auth::{AuthAccountInfo, LocalAuthRequest},
+};
 
 use crate::{
-    data::{database::Database, hash_secret::HashSecret, jwt_secret::JwtSecret},
+    data::{
+        database::Database, hash_secret::HashSecret, jwt_secret::JwtSecret, platforms::steam::Steam,
+    },
     frontend::{
         error::ApiError,
         result::{ApiResponse, ApiResult},
@@ -19,6 +24,7 @@ pub async fn auth_local(
     database: Data<Database>,
     jwt_secret: Data<JwtSecret>,
     hash_secret: Data<HashSecret>,
+    steam: Data<Steam>,
 ) -> ApiResult<AuthAccountInfo> {
     let conn = database.connect()?;
     let accounts = conn.frontend_accounts();
@@ -32,15 +38,49 @@ pub async fn auth_local(
             "The username or password is incorrect"
         ))),
         Some(account) => {
-            // todo
             let permissions = FlagSet::new_truncated(account.permissions);
             let permissions_vec = permissions.into_iter().collect();
             let auth_token =
                 super::create_auth_token(&account, &jwt_secret).map_err(anyhow::Error::msg)?;
 
+            let name = accounts
+                .get_display_names(&[&account])
+                .await?
+                .into_iter()
+                .next()
+                .map(|(_, name)| name)
+                .unwrap_or_else(|| "".to_string());
+
+            let avatar_url = {
+                let provider_connection = accounts.get_provider_connection(account.id).await?;
+
+                match provider_connection {
+                    Some(provider_connection) => match provider_connection.provider {
+                        Provider::Steam => {
+                            let steam_id = provider_connection
+                                .provider_id
+                                .parse::<u64>()
+                                .map_err(anyhow::Error::msg)?;
+                            let user_summary = steam
+                                .get_player_summaries(&[&steam_id])
+                                .await?
+                                .into_iter()
+                                .next();
+
+                            match user_summary {
+                                Some((_, summary)) => Some(summary.avatar_full),
+                                None => None,
+                            }
+                        }
+                        Provider::Epic => None,
+                    },
+                    None => None,
+                }
+            };
+
             ApiResponse::ok(AuthAccountInfo {
-                name: "todo".into(),
-                avatar_url: None,
+                name,
+                avatar_url,
                 game_account_id: account.game_account_id,
                 auth_token,
                 permissions: permissions_vec,
