fa1c8a2f1c92424a6febd8984f384fa2749651a9
Skipcast <420685+Skippeh@users.noreply.github.com>
2023-07-21 09:52:38 +0200

    Fixed not forwarding unknown props to react components

--- parcel-server/frontend/src/index.tsx
+++ parcel-server/frontend/src/index.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
-import styled, { StyleSheetManager } from "styled-components";
+import styled, { StyleSheetManager, WebTarget } from "styled-components";
 import "normalize.css";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import GlobalStyle from "./global_style";
 import isPropValid from "@emotion/is-prop-valid";
+
+const shouldForwardProp = (propName: string, target: WebTarget): boolean => {
+  if (!isPropValid(propName) && typeof target == "string") {
+    console.warn(`Not forwarding prop '${propName}' on`, target);
+    return false;
+  }
+
+  return true;
+};
 
 const rootElement = document.getElementById("root");
 
@@ -17,7 +26,7 @@ if (rootElement == null) {
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <StyleSheetManager shouldForwardProp={isPropValid}>
+    <StyleSheetManager shouldForwardProp={shouldForwardProp}>
       <GlobalStyle />
       <App />
     </StyleSheetManager>
