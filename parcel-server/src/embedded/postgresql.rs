29628cca6f235cd737a19b2c6100a5f4faa2af96
Skipcast <420685+Skippeh@users.noreply.github.com>
2024-07-18 00:21:57 +0200

    Replaced pg-embed crate with postgresql_embedded

--- parcel-server/src/embedded/postgresql.rs
+++ parcel-server/src/embedded/postgresql.rs
@@ -2,15 +2,12 @@ use std::{path::PathBuf, sync::Arc};
 
 use anyhow::Context;
 use futures_util::lock::Mutex;
-use pg_embed::{
-    pg_fetch::{PgFetchSettings, PG_V15},
-    postgres::{PgEmbed, PgSettings},
-};
+use postgresql_embedded::{PostgreSQL, Settings, VersionReq};
 
 use crate::Options;
 
 lazy_static::lazy_static! {
-    static ref PG_EMBED: Arc<Mutex<Option<PgEmbed>>> = Arc::new(Mutex::new(None));
+    static ref PG_EMBED: Arc<Mutex<Option<PostgreSQL>>> = Arc::new(Mutex::new(None));
 }
 
 const DB_NAME: &str = "parcels";
@@ -27,33 +24,26 @@ pub async fn setup_postgresql(args: &Options) -> Result<String, anyhow::Error> {
                     anyhow::bail!("PostgreSQL server is already running")
                 }
                 None => {
-                    let settings: PgSettings = PgSettings {
-                        database_dir: PathBuf::from("data/postgresql/data"),
-                        port: portpicker::pick_unused_port()
-                            .context("Failed to find an unused port")?,
-                        user: "ds".into(),
+                    let settings = Settings {
+                        version: VersionReq::parse("=16.3.0").unwrap(),
+                        installation_dir: PathBuf::from("data/postgresql/bin"),
+                        temporary: false,
+                        data_dir: PathBuf::from("data/postgresql/data"),
                         password: "ds_1234".into(),
-                        auth_method: pg_embed::pg_enums::PgAuthMethod::Plain,
-                        persistent: true,
+                        password_file: PathBuf::from("data/postgresql/pw"),
                         timeout: None,
-                        migration_dir: None,
-                    };
-
-                    let fetch_settings: PgFetchSettings = PgFetchSettings {
-                        version: PG_V15,
                         ..Default::default()
                     };
-
-                    let mut pg_embed = PgEmbed::new(settings, fetch_settings).await?;
+                    let mut pg_embed = PostgreSQL::new(settings);
                     pg_embed.setup().await?;
-                    pg_embed.start_db().await?;
+                    pg_embed.start().await?;
 
                     log::debug!("Creating database if it doesn't exist...");
                     if !pg_embed.database_exists(DB_NAME).await? {
                         pg_embed.create_database(DB_NAME).await?;
                     }
 
-                    let url = pg_embed.full_db_uri(DB_NAME);
+                    let url = pg_embed.settings().url(DB_NAME);
                     static_pg_embed.replace(pg_embed);
                     Ok(url)
                 }
@@ -68,7 +58,7 @@ pub async fn stop_postgresql() -> Result<(), anyhow::Error> {
     match server {
         Some(server) => {
             log::info!("Stopping PostgreSQL server...");
-            server.stop_db().await?;
+            server.stop().await?;
             Ok(())
         }
         None => Ok(()),
